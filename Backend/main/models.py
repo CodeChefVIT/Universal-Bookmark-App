848ced3f97e88c9d5d2034f1d47fe125b2f22fce
hpb1 <harry.prashantb@gmail.com>
2020-05-16 01:56:02 +0530

    Commit - Backend

---
+++ Backend/main/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.db.models import Model 
+from django.contrib.auth.models import User
+import uuid
+# Create your models here.
+
+class URLData(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    url = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
---
+++ Backend/main/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .serializers import(
+    URLDataSerializer
+)
+from .models import(
+    URLData
+)
+from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
+from rest_framework import permissions
+from rest_framework.views import APIView
+
+# Create your views here.
+
+def home(response):
+    return render(response, "home.html", {})
+
+# 200 - status : verified
+# 201 - status : added
+
+class URLAdder(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    parser_classes = [JSONParser]
+
+    def get(self, request):
+        obj = URLData.objects.filter(user=request.user.id)
+        serializer = URLDataSerializer(obj, many=True)
+        return Response(serializer.data, status=200)
+    
+    def post(self, request):
+        request.data['user'] = request.user.id
+        obj = URLDataSerializer(data=request.data)
+        if obj.is_valid():
+            obj.save()
+            return Response({
+                'status' : 'added'
+            },status=201)
+
+
+class URLModifier(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    parser_classes = [JSONParser]
+
+    def post(self, request):
+        try:    
+            obj = URLData.objects.filter(id=request.data['id'])
+            obj.update(url=request.data['url'])
+            return Response(status=202)
+        except:
+            return Response(status=404)
+
+class URLDelete(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    parser_classes = [JSONParser]
+
+    def get(self, request, pk):
+        try:    
+            obj = URLData.objects.filter(id=pk)
+            obj.delete()
+            return Response(status=203)
+        except:
+            return Response(status=404)
