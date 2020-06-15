7f3dff00421c282bcca06e74d465cb1882fa2f31
Ishan Khandelwal <59707330+Ishan-001@users.noreply.github.com>
2020-06-15 14:21:29 +0530

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/example/bookmark/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/bookmark/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.example.item2", appContext.getPackageName());
+        assertEquals("com.example.bookmark", appContext.getPackageName());
     }
 }
