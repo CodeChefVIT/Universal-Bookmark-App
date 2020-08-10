76885e00c047ce9253fa4f0e9265c80c35b97a1d
Ishan <axil.ishan3@gmail.com>
2020-08-10 19:22:57 +0530

    Major bug fixes

--- app/src/main/java/com/codechefvit/bookmarked/Authentication/Register.java
+++ app/src/main/java/com/codechefvit/bookmarked/Authentication/Register.java
@@ -8,7 +8,6 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Gravity;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
